db91b3315a33319f4998a08e22e832d9a252fb7c
wayne.wang <1007677085@qq.com>
2025-12-28 16:20:40 +0800

    调整结构“” ‘’

--- merge_excel.cmd
+++ merge_excel.cmd
@@ -2,17 +2,11 @@
 call %USERPROFILE%\anaconda3\Scripts\activate.bat  %USERPROFILE%\anaconda3
 call conda activate playground
 
-python merge_excel.py data/choice/AU0 K线导出_AU0_5分钟线数据.xlsx
+python merge_excel.py data/choice/disaggregated K线导出_AU0_5分钟线数据.xlsx
 
-python merge_excel.py data/choice/MGC00Y K线导出_MGC00Y_5分钟线数据.xlsx
+python merge_excel.py data/choice/disaggregated K线导出_AG0_5分钟线数据.xlsx
 
-python merge_excel.py data/choice/AG0 K线导出_AG0_5分钟线数据.xlsx
-
-python merge_excel.py data/choice/SI00Y K线导出_SI00Y_5分钟线数据.xlsx
-
-python merge_excel.py data/choice/USDCNH K线导出_USDCNH_5分钟线数据.xlsx
-
-python merge_excel.py data/choice/基准/518850 K线导出_518850_60分钟线数据.xlsx
+python merge_excel.py data/choice/disaggregated K线导出_518850_60分钟线数据.xlsx
 
 call conda deactivate
 
